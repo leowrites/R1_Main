4dcce20e3fe6ab01330e7345d516cdd3d9a8fe0c
leowrites <75044178+leowrites@users.noreply.github.com>
2021-03-11 09:54:31 -0800

    March 11

--- R1_Main.ino
+++ R1_Main.ino
@@ -1,9 +1,3 @@
-#include <MPU9250.h>
-#include <Adafruit_BMP280.h>
-#include <SD.h>
-#include <SPI.h>
-#include <Servo.h>
-
 /*
 
 Software for R1
@@ -12,134 +6,342 @@ Leo Liu
 Feb 19, 2021
 
 */
+#include <MPU9255.h>
+#include <Servo.h>
+#include <Adafruit_BMP280.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Servo.h>
 
-#ifndef YAW_SERVO_PIN
+#define g 9.81
 #define YAW_SERVO_PIN 2
-#endif
-#ifndef PITCH_SERVO_PIN
 #define PITCH_SERVO_PIN 3
-#endif
-#ifndef BUTTON_PIN
-#define BUTTON_PIN 4
-#endif
-#ifndef SERVO_DEFAULT
-#define SERVO_DEFAULT 90
-#endif
-#ifndef SERVO_MAX
-#define SERVO_MAX 100
-#endif
-#ifndef SERVO_MIN
-#define SERVO_MIN 80
-#endif
-#ifndef GLOBAL_DELAY
+#define SDCS 4
+#define BUTTON_PIN 5
+#define servoX_offset 90
+#define servoY_offset 90
+#define SERVO_MAX 130
+#define SERVO_MIN 50
 #define GLOBAL_DELAY 100
-#endif
-#ifndef INITIAL_MASS
 #define INITIAL_MASS 2000
-#endif
-#ifndef MASS_LOSS_RATE
-//per booster for 7 seconds
 #define MASS_LOSS_RATE 5.17
-#endif
-#ifndef INTERVAL_DELAY
-#define INTERVAL_DELAY 20
-#endif
-#ifndef DEFAULT_ORIENTATION
-#define DEFAULT_ORIENTATION 0
-#endif
-#ifndef CS
-#define CS 10 //change when I get the shield
-#endif
-#ifndef CALC_INTERVAL
-#define CALC_INTERVAL 20
-#endif
-#ifndef DATA_INTERVAL
-#define DATA_INTERVAL 100
-#endif
+#define BURN_OUT_TIME 7000
 
-byte xAdj = 0;
-byte yAdj = 0;
 short int currentMass;
-unsigned long LAUNCH_TIME;
+unsigned long launchTime;
 unsigned long timeSinceLaunch;
-unsigned long *prev_ms_calc = 0;
-unsigned long *prev_ms_data = 0;
-//use byte to indicate status to save memory
+
 byte flightStatus = 0;
 bool launched = false;
+bool parachute_depolyed = false;
+bool motor_burn_out = false;
 
-float pGx = 0.0f;
-float pGy = 0.0f;
-float dx = 0.0f;
-float dy = 0.0f;
-//just declare everything global lol Im done with this
-float orientation[1];
-float gyro_rate[1];
-float dG[1];
-float bmp_data[2];
-//delta T
+//PID variables
+double PIDX, PIDY, errorX, errorY, previous_errorX, previous_errorY, pwmX, pwmY, gyroXX, gyroYY;
+//post processing
+double pax, pay, paz, pgx, pgy, pgz;
+//angle output
+int pmw, pmy;
+//error
+int desired_angleX, desired_angleY = 0;
+//pid
+double accAngleX, accAngleY, yaw, pitch, gyroAngleX, gyroAngleY, dt, cur_time, prev_time;
+float servo_gear_ratio = 5.8;
+float pidX_p = 0;
+float pidX_i = 0;
+float pidX_d = 0;
+float pidY_p = 0;
+float pidY_i = 0;
+float pidY_d = 0;
+double kp = 0.13;
+double ki = 0.0;
+double kd = 0.05;
 
-struct servo
-{
-  Servo servoNum;
-  unsigned int pin;
-  int servoRotation;
-  bool hor;
-  bool ver;
-  bool hor_max_confirmed;
-  bool hor_min_confirmed;
-  bool ver_max_confirmed;
-  bool ver_min_confirmed;
-};
+float pressure;
+float attitude;
 
-Servo yawServo;
-Servo pitchServo;
 Adafruit_BMP280 bmpSensor;
-MPU9250 imuSensor;
+MPU9255 mpu;
 File dataFile;
+Servo servoX;
+Servo servoY;
 
-struct servo servo1A
+int preFlightHold()
 {
-  .servoNum = yawServo, .pin = YAW_SERVO_PIN, .servoRotation = SERVO_DEFAULT, .hor = true, .ver = false
-};
-struct servo servo1B
-{
-  .servoNum = pitchServo, .pin = PITCH_SERVO_PIN, .servoRotation = SERVO_DEFAULT, .hor = false, .ver = true
-};
-struct servo servoArray[] = {servo1A, servo1B};
+  //await signal for pre-flight confirmation
+  //bluetooth or a button?
+  int buttonState = digitalRead(BUTTON_PIN);
+  while (buttonState == LOW)
+  {
+    buttonState = digitalRead(BUTTON_PIN);
+    Serial.print(F("Awaiting confirmatiom."));
+  };
+  dataFile.print("Time: ");
+  dataFile.print(millis());
+  dataFile.println(F("Flight confirmed."));
+}
 
-//function prototyping
-bool check_time(unsigned long *last_ms, unsigned short int interval);
-int preFlightHold();
-void preFlightCheck();
-void launchCountDown();
-void ignition();
-void inFlight();
-void gyroReading();
-void bmpDataReading();
-void writeData();
-void adjustmentCalculation();
-void orientationIntegration();
-void proportionalGain();
-void derivativeConvertion();
-void angleOutput();
-void calculate_mass();
-void resetServo();
-void thrustVectorControl(servo servo, bool hor);
-void parachuteDepolyment(float pressure, float attitude);
-void servo_print(short int result, byte servoPin);
-void raise_initialization_error(String msg);
+void launchCountDown()
+{
+  //switch case to avoid blocking code
+  unsigned char tSeconds = 60;
+  while (tSeconds > 0)
+  {
+    tSeconds--;
+    delay(1000);
+  }
+}
+
+void ignition()
+{
+  //outputs power to ignite the rocket motors
+  launchTime = millis();
+  launched = true;
+  flightStatus = 1;
+}
+
+void inFlight()
+{
+  //this will be a data recording loop and transition to parachute depolyment.
+  //calculations will be done in another thread
+  Serial.println(F("TVC Loop"));
+  timeSinceLaunch = millis() - launchTime;
+  if (timeSinceLaunch > BURN_OUT_TIME)
+  {
+    motor_burn_out = true;
+  }
+  if (!motor_burn_out)
+  {
+    calculate_mass();
+  }
+  read_data();
+  process_data();
+  delta_time();
+  calculate_accel_degree();
+  pid();
+  servo_write();
+  pressure = bmpSensor.readPressure();
+  attitude = bmpSensor.readAltitude();
+  write_to_sd();
+  parachuteDepolyment();
+}
+
+void landing()
+{
+  //check y acceleration if it is the same as gravity then stop data recording
+  if (pay < 10 && motor_burn_out == true && parachute_depolyed && true)
+  {
+    dataFile.print("Landing complete. ");
+    dataFile.close();
+  }
+  else
+  {
+    write_to_sd();
+  }
+}
+
+//servo operation
+void reset_servo()
+{
+  servoX.write(servoX_offset);
+  servoY.write(servoY_offset);
+}
+
+double process_acceleration(int input)
+{
+  // process raw acceleration
+  double output = 1;
+
+  //for +- 2g
+  output = input;
+  output = output / 16384;
+  output = output * g;
+
+  return output;
+}
+
+double process_angular_velocity(int16_t input)
+{
+  //process raw gyro rate
+
+  //for +- 250 dps
+  return input / 131;
+}
+
+void delta_time()
+{
+  prev_time = cur_time;
+  cur_time = millis();
+  dt = (cur_time - prev_time) / 1000;
+}
+
+void read_data()
+{
+  Serial.println(F("Reading Data"));
+  mpu.read_acc();
+  mpu.read_gyro();
+  mpu.read_mag();
+}
+
+void process_data()
+{
+  pax = process_acceleration(mpu.ax);
+  pay = process_acceleration(mpu.ay);
+  paz = process_acceleration(mpu.az);
+  pgx = process_angular_velocity(mpu.gx);
+  pgy = process_angular_velocity(mpu.gy);
+  pgz = process_angular_velocity(mpu.gz);
+
+  if (-5 <= pgx && pgx <= 0 || 0 <= pgx && pgx <= 5)
+  {
+    pgx = 0;
+  };
+  if (-5 <= pgy && pgy <= 0 || 0 <= pgy && pgy <= 5)
+  {
+    pgy = 0;
+  };
+  if (-5 <= pgz && pgz <= 0 || 0 <= pgz && pgz <= 5)
+  {
+    pgz = 0;
+  };
+
+  Serial.print(F("AX: "));
+  Serial.print(pax);
+  Serial.print(F("  AY: "));
+  Serial.print(pay);
+  Serial.print(F("  AZ: "));
+  Serial.print(paz);
+  Serial.print(F("      GX: "));
+  Serial.print(pgx);
+  Serial.print(F("  GY: "));
+  Serial.print(pgy);
+  Serial.print(F("  GZ: "));
+  Serial.println(pgz);
+}
+
+void write_to_sd()
+{
+  if (dataFile)
+  {
+    dataFile.print("Time: ");
+    dataFile.println((millis()));
+    dataFile.print(F("AX: "));
+    dataFile.print(pax);
+    dataFile.print(F("  AY: "));
+    dataFile.print(pay);
+    dataFile.print(F("  AZ: "));
+    dataFile.print(paz);
+    dataFile.print(F("      GX: "));
+    dataFile.print(pgx);
+    dataFile.print(F("  GY: "));
+    dataFile.print(pgy);
+    dataFile.print(F("  GZ: "));
+    dataFile.println(pgz);
+    dataFile.print(F("Pressure: "));
+    dataFile.print(pressure);
+    dataFile.print(F("  Attitude: "));
+    dataFile.print(F("Pitch: "));
+    dataFile.print(pitch);
+    dataFile.print(F("    Yaw:"));
+    dataFile.println(yaw);
+    dataFile.println(attitude);
+    dataFile.print(F("PIDX: "));
+    dataFile.println(PIDX);
+  }
+}
+
+void calculate_accel_degree()
+{
+
+  double prevgyroX = pgx;
+  double prevgyroY = pgz;
+
+  gyroAngleX += (((pgx + prevgyroX) / 2) * dt); // deg/s * s = deg
+  gyroAngleY += (((pgz + prevgyroY) / 2) * dt);
+
+  double orientationX = 0.90 * gyroAngleX + 0.10 * accAngleX;
+  double orientationY = 0.90 * gyroAngleY + 0.10 * accAngleY;
+
+  //divided by 32.8 as recommended by the datasheet
+  pitch = orientationX + 2;
+  yaw = orientationY + 2;
+
+  Serial.print(F("Pitch: "));
+  Serial.print(pitch);
+  Serial.print(F("    Yaw:"));
+  Serial.println(yaw);
+}
+
+void pid()
+{
+
+  previous_errorX = errorX;
+  previous_errorY = errorY;
+
+  errorX = pitch - desired_angleX;
+  errorY = yaw - desired_angleY;
+
+  pidX_p = kp * errorX;
+  pidY_p = kp * errorY;
+
+  pidX_d = kd * ((errorX - previous_errorX) / dt);
+  pidY_d = kd * ((errorY - previous_errorY) / dt);
+
+  pidX_i = ki * (pidX_i + errorX * dt);
+  pidY_i = ki * (pidY_i + errorY * dt);
+
+  PIDX = pidX_p + pidX_i + pidX_d;
+  PIDY = pidY_p + pidY_i + pidX_d;
+
+  Serial.print(F("PIDX: "));
+  Serial.println(PIDX);
+
+  //constraint for pwm
+  pwmX = ((PIDX * servo_gear_ratio) + servoX_offset);
+  pwmY = ((PIDY * servo_gear_ratio) + servoY_offset);
+}
+
+void servo_write()
+{
+
+  servoX.write(pwmX);
+  servoY.write(pwmY);
+}
+
+void calculate_mass()
+{
+  currentMass = INITIAL_MASS - (timeSinceLaunch / 1000 * MASS_LOSS_RATE);
+  dataFile.print("Mass: ");
+  dataFile.println(currentMass);
+}
+
+void parachuteDepolyment()
+{
+  //cross reference y acceleration and motor burn out time
+  if (motor_burn_out)
+  {
+    parachute_depolyed = true;
+    dataFile.print("Motor burn out, Time: ");
+    dataFile.println(millis());
+  }
+}
+
+void raise_initialization_error(String msg)
+{
+  Serial.println(msg + F(" initialization failed"));
+  while (1)
+    ;
+}
 
 void setup()
 {
   // put your setup code here, to run once:
+  pinMode(BUTTON_PIN, INPUT);
+
   Serial.begin(9600);
   Serial.println(F("System initializing"));
-  Wire.begin();
-
-  pinMode(BUTTON_PIN, INPUT);
-  yawServo.attach(YAW_SERVO_PIN);
-  pitchServo.attach(PITCH_SERVO_PIN);
+  servoX.attach(YAW_SERVO_PIN);
+  servoY.attach(PITCH_SERVO_PIN);
 
   currentMass = INITIAL_MASS;
 
@@ -152,7 +354,7 @@ void setup()
     Serial.println(F("BMP initialization successful."));
   }
 
-  if (!imuSensor.setup(0x68))
+  if (mpu.init())
   {
     raise_initialization_error(F("IMU sensor"));
   }
@@ -160,7 +362,7 @@ void setup()
   {
     Serial.println(F("IMU initialization successful."));
   }
-  if (!SD.begin(CS))
+  if (!SD.begin(SDCS))
   {
     raise_initialization_error(F("SD card"));
   }
@@ -173,261 +375,31 @@ void setup()
 
   if (!dataFile)
   {
-    raise_initialization_error(F("SD card file "));
+    raise_initialization_error(F("SD card file writing"));
   }
 
   dataFile.println(F("------------ this is the beginning of the data recording ------------"));
-}
-
-bool check_time(unsigned long *last_ms, unsigned short int interval)
-{
-  if (millis() - *last_ms >= interval)
-  {
-    *last_ms = millis();
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
-
-int preFlightHold()
-{
-  //await signal for pre-flight confirmation
-  //bluetooth or a button?
-  int buttonState = digitalRead(BUTTON_PIN);
-  bool message_sent = false;
-  while (buttonState == LOW)
-  {
-    buttonState = digitalRead(BUTTON_PIN);
-  }
-  Serial.println(F("Flight confirmed"));
-}
-
-void preFlightCheck()
-{
-  //check the rotation of the servos, readings from bmp and imu, and sd card recording, recalibrate bmp and imu
-  resetServo();
-  Serial.println(F("IMU calibration begin."));
-  imuSensor.verbose(true);
-  imuSensor.calibrateAccelGyro();
-  imuSensor.calibrateMag();
-}
-
-void launchCountDown()
-{
-  //switch case to avoid blocking code
-  unsigned char tSeconds = 60;
-  while (tSeconds > 0)
-  {
-    tSeconds--;
-    delay(1000);
-    if (tSeconds == 5)
-    {
-      //initiate calculation thread and tvc at t-5
-      adjustmentCalculation();
-    }
-  }
-  //activate the inFlight loop in a seperate thread at T-1
-}
-
-void ignition()
-{
-  //outputs power to ignite the rocket motors
-  LAUNCH_TIME = millis();
-  launched = true;
-}
-
-void inFlight()
-{
-  //this will be a data recording loop and transition to parachute depolyment.
-  //calculations will be done in another thread
-  while (true)
-  { //replace with pressure sensor
-  }
-}
-
-//data reading
-
-void gyroReading()
-{
-  //return in rad/20ms
-  float dx = imuSensor.getGyroX();
-  float dy = imuSensor.getGyroY();
-  dx = 20 * dx / 1000;
-  dy = 20 * dy / 1000;
-
-  gyro_rate[0] = dx;
-  gyro_rate[1] = dy;
-}
-
-void bmpDataReading()
-{
-  bmp_data[0] = bmpSensor.readPressure();
-  bmp_data[1] = bmpSensor.readAltitude();
-  bmp_data[2] = bmpSensor.readTemperature();
-}
-
-//data recording
-void writeData()
-{
   dataFile.println(millis());
-  dataFile.print(F("X rate of change: "));
-  dataFile.println(gyro_rate[0]);
-  dataFile.print(F("Y rate of change: "));
-  dataFile.println(gyro_rate[1]);
-}
-
-//data processing
-
-void adjustmentCalculation()
-{
-  //updates the adjustment angles
-  //current orientation (same as error)
-  //loop executes once every 20 miliseconds
-  //the array returning probably need fixing
-  gyroReading();
-  orientationIntegration();
-  proportionalGain();
-  derivativeConvertion();
-  angleOutput();
-  //call tvc
-  thrustVectorControl(servo1A, servo1A.hor);
-  thrustVectorControl(servo1B, servo1B.hor);
-}
-
-void orientationIntegration()
-{
-  //integrate radian/s with respect to mili seconds
-  //differentiate gain and lost
-  orientation[0] += gyro_rate[0];
-  orientation[1] += gyro_rate[1];
-}
-
-void proportionalGain()
-{
-  pGx = orientation[0] * 0.6;
-  pGy = orientation[1] * 0.6;
-}
-
-void derivativeConvertion()
-{
-  dx = gyro_rate[0] * 0.3;
-  dy = gyro_rate[1] * 0.3;
-}
-
-void angleOutput()
-{
-  xAdj = pGx + dx;
-  yAdj = pGy + dy;
-}
-
-void calculate_mass()
-{
-  //calculates mass
-  timeSinceLaunch = millis() - LAUNCH_TIME;
-  currentMass = INITIAL_MASS - MASS_LOSS_RATE * timeSinceLaunch;
-}
-
-//servo operation
-
-void resetServo()
-//resets the rotation of all servos
-{
-  for (size_t i = 0; i < sizeof(servoArray) / sizeof(servoArray[0]); i++)
-  {
-    servo servo = servoArray[i];
-    servo.servoRotation = servo.servoNum.read();
-    if (servo.servoRotation > 90)
-    {
-      while (servo.servoRotation > 90)
-      {
-        servo.servoRotation--;
-        servo.servoNum.write(servo.servoRotation);
-      }
-    }
-    else if (servo.servoRotation < 90)
-    {
-      while (servo.servoRotation < 90)
-      {
-        servo.servoRotation++;
-        servo.servoNum.write(servo.servoRotation);
-      }
-    }
-    delay(GLOBAL_DELAY);
-  }
-  Serial.println("Servos at default.");
-}
-
-void thrustVectorControl(servo servo, bool hor)
-{
-  //controls indiviudal servos
-  //move to exact position
-  servo.servoRotation = servo.servoNum.read();
-  if (hor)
-  {
-    short int current = servo.servoNum.read();
-    short int result = current + xAdj;
-    servo.servoNum.write(result);
-    servo_print(result, servo.pin);
-  }
-  else
-  {
-    short int current = servo.servoNum.read();
-    short int result = current + yAdj;
-    servo.servoNum.write(result);
-    servo_print(result, servo.pin);
-  }
-}
-
-//parachute
-
-void parachuteDepolyment(float pressure, float attitude)
-{
-  //cross reference
-}
-
-//feedback to user
-
-void servo_print(short int result, byte servoPin)
-{
-  Serial.print(F("Servo:"));
-  Serial.print(servoPin);
-  Serial.print(F(" rotation: "));
-  Serial.println(result);
-}
-
-void raise_initialization_error(String msg)
-{
-  Serial.println(msg + F(" initialization failed"));
 }
 
 void loop()
 {
-  //state manager
-  //switch
-  if (!launched)
+  while (!launched)
   {
     preFlightHold();
-    preFlightCheck();
     launchCountDown();
     ignition();
-    flightStatus = 1; // 1 = inflight, 0 = not in flight
-    while (flightStatus = 1)
+  }
+  while (flightStatus = 1)
+  {
+    inFlight();
+    if (parachute_depolyed)
     {
-      if (check_time(prev_ms_calc, CALC_INTERVAL))
-      {
-        adjustmentCalculation();
-      }
-      if (check_time(prev_ms_data, DATA_INTERVAL))
-      {
-        //record data function
-      }
+      flightStatus = 2;
     }
-    float pressure = bmpSensor.readPressure();
-    float attitude = bmpSensor.readAltitude();
-    parachuteDepolyment(pressure, attitude);
-    dataFile.close();
+  }
+  while (flightStatus = 2)
+  {
+    landing();
   }
 }
